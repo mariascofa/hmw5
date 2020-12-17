9eb9dac0b584cce09b21a1365c5ae39056313bb4
Maria Scofa <mariascofa@gmail.com>
2020-12-17 07:13:24 +0200

    7th Homework

--- home/views.py
+++ home/views.py
@@ -1,18 +1,24 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from home.forms import StudentForm
 from home.models import Student
 
 
 def show_all_students(request):
-    """This function will return all the students from the
-     model in the template."""
     students = Student.objects.all()
     return render(
         request=request,
         template_name='index.html',
-        context={
-            'Students': students,
-        }
-    )
+        context={'Students': students, })
 
+def create_students(request):
+    if request.method == "GET":
+        student_form = StudentForm()
+        context = {"form": student_form, }
+        return render(request, "form_index.html", context=context)
+    elif request.method == "POST":
+        student_form = StudentForm(request.POST)
+        if student_form.is_valid():
+            student_form.save()
+        return redirect('/students')
 
